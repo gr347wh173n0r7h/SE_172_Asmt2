c5c21c1fb710fbef2818693aba087b0703c9ad20
gr347wh173n0r7h <jordan.a.petersen@gmail.com>
2016-03-11 14:44:58 -0800

    Part2

--- index.js
+++ index.js
@@ -11,11 +11,27 @@ app.use(express.static(__dirname + '/public'))
 
 /*serves main page*/
 app.get('/', function(request, response) {
-// response.sendfile('index.html')
-    var data = fs.readFileSync('index.html')
-    response.send(data.toString())
 
-})
+	fs.open('index.html', 'r', function(err, fd) {
+    fs.fstat(fd, function(err, stats) {
+        var bufferSize=stats.size,
+            chunkSize=512,
+            buffer=new Buffer(bufferSize),
+            bytesRead = 0;
+
+        while (bytesRead < bufferSize) {
+            if ((bytesRead + chunkSize) > bufferSize) {
+                chunkSize = (bufferSize - bytesRead);
+            }
+            fs.read(fd, buffer, bytesRead, chunkSize, bytesRead);
+            bytesRead += chunkSize;
+        }
+        response.send(buffer.toString('utf8', 0, bufferSize));
+        fs.close(fd);
+    });
+});
+
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
